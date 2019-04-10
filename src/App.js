51fe779f70f1fe3a9f6da6b4b952f94b6b5bd68e
saileshkasaju <leosailesh@gmail.com>
2019-04-10 22:00:24 +0545

    time-trial syntax web dev

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  const [joke, setJoke] = useState({ joke: '' });
+  const api = 'https://icanhazdadjoke.com';
+  const getData = async () =>
+    fetch(api, { headers: { accept: 'application/json' } })
+      .then(data => data.json())
+      .then(data => setJoke(data));
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      {joke.joke}
+      <button onClick={getData}>Get new joke</button>
+    </div>
+  );
 }
-
-export default App;
